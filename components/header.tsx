c24a3c7f9b688165d47cfc4c9acc5d767d787cd3
michaelyuhe <2312744987@qq.com>
2024-01-12 13:59:54 +0800

    add GitHub link

--- components/header.tsx
+++ components/header.tsx
@@ -7,6 +7,13 @@ export default function Header() {
         <Link className="text-3xl font-bold text-white" href="/">
           rabbit it
         </Link>
+
+        <Link
+          href="https://github.com/michaelyuhe/rabbit-it"
+          className="text-xl font-bold text-white"
+        >
+          GitHub
+        </Link>
       </div>
     </header>
   );
