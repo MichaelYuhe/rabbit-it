bc9b7b53739c77d2e1598e37f1292d220f931297
michaelyuhe <2312744987@qq.com>
2024-01-12 16:12:30 +0800

    fix utm

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="flex w-screen justify-center border-t-2 border-gray-600 py-4">
       <div className="w-full max-w-6xl">
-        <Link href="https://zeabur.com?referralCode=MichaelYuhe&utm_source=nice-buttons&utm_campaign=oss">
+        <Link href="https://zeabur.com?referralCode=MichaelYuhe&utm_source=rabbit_it&utm_campaign=oss">
           <img
             src="https://zeabur.com/deployed-on-zeabur-light.svg"
             alt="zeabur"
